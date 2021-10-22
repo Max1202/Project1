ef866782f85520d68379309241abf3eb63948dca
Max1202 <melnikovmaks1202@gmail.com>
2021-10-22 16:33:26 +0300

    First

---
+++ CA2.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+#include "math.h"
+
+using namespace std;
+
+int main()
+{
+	int xmax = 6, xmin = 3;
+	float h;
+	cout << "Enter h =";
+	cin >> h;
+	float y, x;
+	while (x<=xmax)
+	{
+		y = tan(x*x)-3+2*x;
+		cout << setw(10) << "x=" << x << setw(10) << "y=" << y << endl;
+		x += h;
+	}
+	
+}
